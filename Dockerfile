a0abb1dea92adcc6a668cd8ece797a9514695efa
Kevin McKenzie <kevinmckenzie@kevinmckenzie-macbookpro.roam.corp.google.com>
2019-04-28 08:23:13 -0700

    moved flask secret to gcs

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,8 @@
 FROM python:3.7
 MAINTAINER Kevin McKenzie "kjtmckenzie@gmail.com"
 COPY . /main
-WORKDIR /app
+WORKDIR /main
 RUN pip install -r requirements.txt
 ENV GOOGLE_APPLICATION_CREDENTIALS kjtmckenzie-home-fs-e95cb6b832bf.json
-CMD exec gunicorn --bind 8080:8080 --workers 1 --threads 8 main:app
+CMD exec gunicorn --bind :$PORT --workers 1 --threads 8 main:app
+
--- settings.py
+++ settings.py
@@ -4,29 +4,40 @@
 # consent from Kevin McKenzie.
 
 import os
-from google.cloud import firestore
+from google.cloud import firestore, storage
 
 PROJECT = "kjtmckenzie-home-fs"
+CONFIG_BUCKET = 'kjtmckenzie-home-fs-config'
+PROD_FLASK_SECRET = "flask_secret.txt"
+DEV_FLASK_SECRET = "dev_flask_secret.txt"
+FIREGASE_API_KEY = "firebase_api_key.txt"
 PROVIDERS = "google,email"
-FIREBASE_API_KEY = 'AIzaSyD4Q3PltZ6qSYlNWDv2P3ekDIqwwv-nMaI'
-
 UPLOAD_BUCKET = "kjtmckenzie-home-fs.appspot.com"
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
 
-def init(app):
-    app.secret_key = open("flask_secret.txt", 'rb').read()
-    app.config['db'] = firestore.Client(project=PROJECT)
 
-    app.config['FIREBASE_API_KEY'] = FIREBASE_API_KEY
+def get_secret(filename):
+    storage_client = storage.Client()
+    bucket = storage_client.get_bucket(CONFIG_BUCKET)
+    blob = bucket.get_blob(filename)
+    return blob.download_as_string()
+
+def init(app):
+    app.config['db'] = firestore.Client(project=PROJECT)
+    
     app.config['FIREBASE_PROJECT_ID'] = PROJECT
     app.config['FIREBASE_AUTH_SIGN_IN_OPTIONS'] = PROVIDERS
 
     if os.getenv('GAE_ENV', '').startswith('standard'):
+        app.secret_key = get_secret(PROD_FLASK_SECRET)
+        app.config['FIREBASE_API_KEY'] = get_secret(FIREGASE_API_KEY)
         app.config['IS_DEV'] = False
         app.debug = False
         app.config['UPLOAD_BUCKET'] = UPLOAD_BUCKET
     else:
         app.config['IS_DEV'] = True
+        app.config['FIREBASE_API_KEY'] = "dev"
+        app.secret_key = open(DEV_FLASK_SECRET, 'rb').read()
         app.debug = True
         app.config['UPLOAD_BUCKET'] = "dev_uploads"
 
